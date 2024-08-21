682cbcf9d1197bec032c94772c946a6b4bcebea7
Matthias02 <matthiasm2016@gmail.com>
2024-08-21 16:18:27 +0200

    frontPage created

---
+++ app/_components/Header.jsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+
+  return (
+    <div className='p-5 bg-gray-50'>
+        <div className='flex justify-between'>
+            <div className='flex items-center gap-6'>
+                <Image src='/logo.svg' width={180} height={180} alt='logo'/>
+                <ul className='flex gap-3 capitalize'>
+                    <Link href={'/explore'}>
+                      <li className='hover:text-[#AC8049] cursor-pointer'>explore</li> 
+                    </Link>
+                    <Link href={'/contact'}>
+                      <li className='hover:text-[#AC8049] cursor-pointer'>contact</li>
+                    </Link>
+                </ul>
+            </div>
+            <Button className='hover:bg-[#AC8049] hover:text-white'>Get started</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
